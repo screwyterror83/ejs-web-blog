73cd92e6ed6599f1d4c9e188601c319ac8b23d2a
screwyterror83 <screwyterror83@gmail.com>
2020-04-16 01:16:43 -0700

    add "about" and "contact" page content

--- app.js
+++ app.js
@@ -16,8 +16,15 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", { startContent: homeStartingContent });
-})
+});
 
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactPageContent: contactContent });
+});
 
 
 
