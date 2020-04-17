14d60273f821011523022136f790bd5b19c04b4b
screwyterror83 <screwyterror83@gmail.com>
2020-04-17 01:17:27 -0700

    keep working on functionality of the blog

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash")
 // const post = require(__dirname + "/post.js");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -24,6 +25,8 @@ const posts = []
 
 /* Home page content */
 app.get("/", (req, res) => {
+
+  
   res.render("home", {
     startContent: homeStartingContent, newPosts:posts
   });
@@ -60,14 +63,30 @@ app.post("/compose", (req, res) => {
     }
   };
 
+  
+
   const post = new Post(req.body.postTitle, req.body.postBody);
 
   posts.push(post);
-  // console.log(post);
-  // console.log(posts);
 
   res.redirect("/");
   
+  
+});
+
+app.get("/post/:postName", (req, res) => {
+  
+  console.log(req.params)
+
+  const postName = _.kebabCase(req.params.postName);
+
+  posts.forEach((post) => {
+    if (_.kebabCase(post.title) === postName) {
+      console.log("Match Found");
+    } else {
+      console.log("There is no such post");
+    }
+  });
   
 })
 
